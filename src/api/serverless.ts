a3e636b2d4010f1a87525ad8fe9d459822951d27
viktorhugodev <viktorhugo.dev@gmail.com>
2022-11-28 23:10:16 -0300

    try deploy vercel

--- src/api/serverless.ts
+++ src/api/serverless.ts
@@ -10,7 +10,7 @@ const app = Fastify({
 });
 
 // Register your application as a normal plugin.
-app.register(import("./app"), {
+app.register(import("../functions/index"), {
     prefix: '/'
 });
 
